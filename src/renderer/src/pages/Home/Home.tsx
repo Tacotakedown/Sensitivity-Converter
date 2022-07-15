c1a4557776f702e06b4619f0e36ab8a369569fdf
Tacotakedown <87836366+Tacotakedown@users.noreply.github.com>
2022-07-15 15:06:46 -0600

    chore; fix api calls

--- src/renderer/src/pages/Home/Home.tsx
+++ src/renderer/src/pages/Home/Home.tsx
@@ -1,41 +1,56 @@
 import { Component, createSignal } from 'solid-js';
 import GoveeLED from '../../Common/fetch/goveeApi';
+import superagent from 'superagent';
+import axios from 'axios';
 import { HexColorInput, HexColorPicker } from 'solid-colorful';
 import DeviceGrid from '@/Common/components/device/deviceGrid';
 import Device from '@/Common/components/device/device';
 
-// const headers = new Headers({
-// 	'Govee-API-Key': '5d0bd033-4261-40fa-8ebe-0c3044489d8d',
-// });
-// const request = new Request('https://developer-api.govee.com/v1/devices', {
-// 	method: 'GET',
-// 	headers: headers,
-// 	mode: 'no-cors',
-// 	cache: 'default',
-// });
-
-// fetch(request).then((res) => console.log(res));
-
-const GoveeClient = new GoveeLED({
-	apiKey: '5d0bd033-4261-40fa-8ebe-0c3044489d8d',
-	mac: '',
-	model: '',
-});
-
-GoveeClient.getDevices().then((data) => console.log(data));
-
 const Home = () => {
 	const [color, setColor] = createSignal('#aabbcc');
+	const [data, setData] = createSignal('');
+	// const headers = new Headers({
+	// 	'Govee-API-Key': '5d0bd033-4261-40fa-8ebe-0c3044489d8d',
+	// });
+
+	// const request = new Request('https://cors-anywhere.herokuapp.com/https://developer-api.govee.com/v1/devices', {
+	// 	method: 'GET',
+	// 	headers: headers,
+	// 	mode: 'no-cors',
+	// 	cache: 'default',
+	// });
+
+	// fetch(request)
+	// 	.then((res) => setData(res.toString()))
+	// 	.catch((error) => {
+	// 		console.error(error);
+	// 	});
+	// headers: {
+	// 	'Content-Type': 'application/json',
+	// 	'Govee-API-Key': '5d0bd033-4261-40fa-8ebe-0c3044489d8d',
+	// 	'Access-Control-Allow-Origin': '*',
+	// 	'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,PATCH,OPTIONS',
+	// 	'Access-Control-Allow-Credentials': true,
+	// },
+
+	superagent
+		.get(
+			'https://cors-anywhere.herokuapp.com/https://developer-api.govee.com/v1/devices'
+		)
+		.set('Govee-API-Key', '5d0bd033-4261-40fa-8ebe-0c3044489d8d')
+		.then((res) => {
+			console.log(res.body.message);
+			setData(res.body.message);
+		})
+		.catch(console.error);
 
 	return (
 		<>
 			<div className="text-white">
 				All Devices:
 				<DeviceGrid>
-					<Device dev="2" />
-					<Device dev={29} />
-					<Device dev={89} />
-					<Device dev={50} />
+					<Device dev="1" />
+					{data()}
 				</DeviceGrid>
 			</div>
 		</>
